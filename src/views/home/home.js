d5cefa4f5d14e845a96803999a721e009fbefaf2
bmonroyg <bmonroyg@emeal.nttdata.com>
2022-09-09 12:13:46 -0500

    register finish

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,8 +1,13 @@
 import {LitElement,html} from 'lit-element';
 import './tabla';
 import './registro';
-
 export class Home extends LitElement {
+  static get properties(){
+    return {
+      lista: {type: Array}
+    };
+  };
+
   constructor(){
     super();
     this.lista = [
@@ -12,11 +17,17 @@ export class Home extends LitElement {
     ];
   }
 
+  agregarAlista(event){
+    let newLista = Object.assign([],this.lista);
+    newLista.push(event.detail);
+    this.lista = newLista;
+  }
+
   render() {
     return html`
     <div>
       <h1>Home</h1>
-      <wc-registro></wc-registro>
+      <wc-registro @guardarPersona="${this.agregarAlista}"></wc-registro>
       <wc-tabla .lista="${this.lista}"></wc-tabla>
     </div>
     `;
--- src/views/home/registro.js
+++ src/views/home/registro.js
@@ -3,18 +3,49 @@ import {LitElement,html} from 'lit-element';
 export class Registro extends LitElement {
     static get properties(){
       return {
-        lista: {type: Array}
+        nombre: {type: String},
+        apellido: {type: String},
+        edad: {type: Number}
       };
     }
+
+    constructor(){
+      super();
+      this.nombre = '';
+      this.apellido = '';
+      this.edad = '';
+    }
+
+    manejador(nombre){
+      return (event)=>{
+        let valor = event.target.value;
+        this[nombre] = valor;
+      };
+    }
+
+    guardar(){
+      let detail = {
+        nombre: this.nombre,
+        apellido: this.apellido,
+        edad: this.edad
+      };
+      let opciones = {
+        bubbles: true,
+        detail: detail,
+        composed: true
+      };
+      this.dispatchEvent(new CustomEvent('guardarPersona', opciones));
+    }
+
     render(){
       return html`
 
         <h1>Registrar Datos</h1>
         <form>
-          <label>Nombre<input/></label>
-          <label>Apellido<input/></label>
-          <label>Edad<input/></label>
-          <button type="button">Registrar</button>
+          <label>Nombre<input @input="${this.manejador('nombre')}" .value="${this.nombre}"/></label>
+          <label>Apellido<input @input="${this.manejador('apellido')}" .value="${this.apellido}"/></label>
+          <label>Edad<input @input="${this.manejador('edad')}" .value="${this.edad}"/></label>
+          <button @click="${this.guardar}" type="button">Guardar</button>
         </form>
       `;
     };
